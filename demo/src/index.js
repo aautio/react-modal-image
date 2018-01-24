1fddc684933e0543176d1e908edcc594c9849610
Ari Autio <ari.autio@iki.fi>
2018-01-24 18:26:56 +0200

    Removed react-live from the demo to keep things lightweight

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,37 +3,31 @@ import { render } from "react-dom";
 
 import ModalImage from "../../src";
 
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
-
 class Demo extends Component {
   render() {
-    const scope = { ModalImage };
+    return (
+      <div>
+        <h2>You start with the imports</h2>
+        <pre>import ModalImage from 'react-modal-image';</pre>
 
-    const demoCode = `<ModalImage
+        <h2>then set up the component</h2>
+
+        <pre>{`<ModalImage
   alt="Here is the caption"
   preview="https://picsum.photos/200/200?image=0"
   fullscreen="https://picsum.photos/1024/1024?image=0"
   download="https://picsum.photos/2048/2048?image=0"
 />
-`;
+      `}</pre>
 
-    return (
-      <LiveProvider scope={scope} code={demoCode}>
-        <h2>You start with the imports</h2>
-        <pre>import ModalImage from 'react-modal-image';</pre>
-
-        <h2>then set up the component</h2>
-        <LiveEditor style={{ marginTop: "20px", marginBottom: "20px" }} />
-        <LiveError
-          style={{
-            marginTop: "20px",
-            marginBottom: "20px",
-            padding: "20px",
-            backgroundColor: "lightpink"
-          }}
-        />
         <h2>and try it out in action </h2>
-        <LivePreview />
+
+        <ModalImage
+          alt="Here is the caption"
+          preview="https://picsum.photos/200/200?image=0"
+          fullscreen="https://picsum.photos/1024/1024?image=0"
+          download="https://picsum.photos/2048/2048?image=0"
+        />
         <p>^ click the image above</p>
 
         <h2>Further info</h2>
@@ -43,10 +37,6 @@ class Demo extends Component {
           <a href="https://github.com/aautio/react-modal-image">Github</a>
         </p>
         <p>
-          The live editor is made with{" "}
-          <a href="https://github.com/FormidableLabs/react-live">react-live</a>.
-        </p>
-        <p>
           Images for the demo are provided by{" "}
           <a href="https://picsum.photos/">Lorem Picsum</a>.
         </p>
@@ -54,7 +44,7 @@ class Demo extends Component {
           Everything is put together with{" "}
           <a href="https://github.com/insin/nwb">nwb</a>.
         </p>
-      </LiveProvider>
+      </div>
     );
   }
 }
