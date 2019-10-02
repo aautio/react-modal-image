e2198b3bc16d2a178ab6723175fa43054a9a47da
Ari Autio <ari.autio@iki.fi>
2019-10-02 17:39:24 +0300

    Prevent material-ui overlow with z-index increase. Fixes #9

--- src/styles.js
+++ src/styles.js
@@ -33,7 +33,7 @@ export const lightboxStyles = ({ imageBackgroundColor }) => `
 
   .__react_modal_image__modal_container {
     position: fixed;
-    z-index: 1000;
+    z-index: 5000;
     left: 0;
     top: 0;
     width: 100%;
