677b72442d3f674ee333e8ca86f69f2043c3fe0e
Ari Autio <ari.autio@iki.fi>
2022-09-08 17:41:10 +0300

    Fix download links to cross-origin images
    Fixes #25. Cross-origin images are downloaded with fetch.
    
    Preparing to publish `2.6.0` soon once additional fixes are ready.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,90 +1,99 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React from "react";
+import { createRoot } from 'react-dom/client';
 
 import ModalImage from "../../src";
 
-class Demo extends Component {
-  render() {
-    return (
-      <div>
-        <h1>react-modal-image</h1>
+import pkg from "../../package.json"
 
-        <h2>#1 with alt, small, medium and large props</h2>
+const Demo = () => (
+  <div>
+    <h1>Demo of react-modal-image@{pkg.version}</h1>
 
-        <div style={{ maxWidth: "400px" }}>
-          <ModalImage
-            alt="Here is the caption"
-            small="example_img_small.jpg"
-            medium="example_img_medium.jpg"
-            large="example_img_large.jpg"
-          />
-        </div>
-        <p>^ click or inspect the image above</p>
+    <h2>#1 with alt, small, medium and large props</h2>
 
-        <h2>#2 with small and large props defined only</h2>
+    <div>
+      <ModalImage
+        alt="Here is the caption"
+        small="example_img_small.jpg"
+        medium="example_img_medium.jpg"
+        large="example_img_large.jpg"
+      />
+    </div>
+    <p>^ click or inspect the image above</p>
 
-        <div style={{ maxWidth: "400px" }}>
-          <ModalImage
-            small="example_img_small.jpg"
-            large="example_img_large.jpg"
-          />
-        </div>
-        <p>^ click or inspect the image above</p>
+    <h2>#2 with small and large props defined only</h2>
 
-        <h2>#3 with small and medium props defined only</h2>
+    <div>
+      <ModalImage
+        small="example_img_small.jpg"
+        large="example_img_large.jpg"
+      />
+    </div>
+    <p>^ click or inspect the image above</p>
 
-        <div style={{ maxWidth: "400px" }}>
-          <ModalImage
-            small="example_img_small.jpg"
-            medium="example_img_medium.jpg"
-          />
-        </div>
-        <p>^ click or inspect the image above</p>
+    <h2>#3 with small and medium props defined only</h2>
 
-        <h2>#4 with download and zoom -buttons hidden</h2>
+    <div>
+      <ModalImage
+        small="example_img_small.jpg"
+        medium="example_img_medium.jpg"
+      />
+    </div>
+    <p>^ click or inspect the image above</p>
 
-        <div style={{ maxWidth: "400px" }}>
-          <ModalImage
-            small="example_img_small.jpg"
-            large="example_img_large.jpg"
-            hideDownload={true}
-            hideZoom={true}
-          />
-        </div>
-        <p>^ click or inspect the image above</p>
+    <h2>#4 with download and zoom -buttons hidden</h2>
 
-        <h2>#5 with transparent png shown in hotpink background</h2>
+    <div>
+      <ModalImage
+        small="example_img_small.jpg"
+        large="example_img_large.jpg"
+        hideDownload={true}
+        hideZoom={true}
+      />
+    </div>
+    <p>^ click or inspect the image above</p>
 
-        <div style={{ maxWidth: "400px" }}>
-          <ModalImage
-            small="example_transparent_heart.png"
-            large="example_transparent_heart.png"
-            hideDownload={true}
-            hideZoom={true}
-            imageBackgroundColor="hotpink"
-          />
-        </div>
-        <p>^ click or inspect the image above</p>
+    <h2>#5 with transparent png shown in hotpink background</h2>
 
-        <h2>#6 with rotation -button displayed</h2>
+    <div>
+      <ModalImage
+        small="example_transparent_heart.png"
+        large="example_transparent_heart.png"
+        hideDownload={true}
+        hideZoom={true}
+        imageBackgroundColor="hotpink"
+      />
+    </div>
+    <p>^ click or inspect the image above</p>
 
-        <div style={{ maxWidth: "400px" }}>
-          <ModalImage
-            small="example_img_small.jpg"
-            large="example_img_large.jpg"
-            showRotate={true}
-          />
-        </div>
-        <p>^ click or inspect the image above</p>
+    <h2>#6 with rotation -button displayed</h2>
 
-        <h2>Further info</h2>
+    <div>
+      <ModalImage
+        small="example_img_small.jpg"
+        large="example_img_large.jpg"
+        showRotate={true}
+      />
+    </div>
+    <p>^ click or inspect the image above</p>
 
-        <p>
-          <a href="https://github.com/aautio/react-modal-image">Github</a>
-        </p>
-      </div>
-    );
-  }
-}
+    <h2>#7 with images from external domain</h2>
 
-render(<Demo />, document.querySelector("#demo"));
+    <div>
+      <ModalImage
+        small="https://dummyimage.com/420x200/000/aaa"
+        large="https://dummyimage.com/640x360/000/aaa"
+      />
+    </div>
+
+    <h2>Further info</h2>
+
+    <p>
+      <a href="https://github.com/aautio/react-modal-image">Github</a>
+    </p>
+  </div>
+);
+
+// @ts-ignore
+const root = createRoot(document.querySelector("#demo"));
+root.render(<Demo />);
